1f5c5874901117d465b569af03925f5eb07193dd
olivierdalang <olivier.dalang@gmail.com>
2019-08-20 12:25:33 +0200

    better entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,43 +3,47 @@ set -e
 
 echo "Running entrypoint at $(date '+%Y/%m/%d %H:%M:%S')..."
 
-# If no certs exist, we create self-signed certs, as nginx/other may refuse to start
-# without certificates files, preventing them from serving the challenges.
-if [ ! -f /etc/letsencrypt/live/default/privkey.pem ] ||  [ ! -f /etc/letsencrypt/live/default/cert.pem ]; then
-    echo "No letsencrypt certificates found. We need self-signed fallback certificates."
-    if [ ! -f /etc/letsencrypt/self-signed/privkey.pem ] ||  [ ! -f /etc/letsencrypt/self-signed/cert.pem ]; then
-        echo "No self-signed certificates found. We will create some"
-        mkdir -p /etc/letsencrypt/self-signed
-        openssl req \
-            -new \
-            -newkey rsa:4096 \
-            -days 365 \
-            -nodes \
-            -x509 \
-            -subj "/C=XX/ST=XXX/L=XXX/O=XXX/CN=${DOMAIN}" \
-            -keyout /etc/letsencrypt/self-signed/privkey.pem \
-            -out /etc/letsencrypt/self-signed/cert.pem
-        ln -fs ./self-signed/privkey.pem /etc/letsencrypt/privkey.pem 
-        ln -fs ./self-signed/cert.pem /etc/letsencrypt/cert.pem 
-    else
-        echo "Self-signed certificates already exist."
-    fi
+# Assert configuration is correct
+if [ "$MODE" != "disabled" ] && [ "$MODE" != "staging" ] && [ "$MODE" != "production" ]; then
+    echo "ERROR ! MODE must be one of 'disabled', 'staging' or 'production'."
+    exit 1
 fi
+
+# We create self-signed certs as fallback while we get actual certificates from letsencrypt,
+# as missing certs may prevent webservers from starting and thus serving the challenges.
+if [ ! -f /etc/letsencrypt/self-signed/privkey.pem ] ||  [ ! -f /etc/letsencrypt/self-signed/cert.pem ]; then
+    echo "No self-signed certificates found. Generating self-signed certificates..."
+    mkdir -p /etc/letsencrypt/self-signed
+    openssl req \
+        -new \
+        -newkey rsa:4096 \
+        -days 365 \
+        -nodes \
+        -x509 \
+        -subj "/C=XX/ST=XXX/L=XXX/O=XXX/CN=${DOMAIN}" \
+        -keyout /etc/letsencrypt/self-signed/privkey.pem \
+        -out /etc/letsencrypt/self-signed/cert.pem
+else
+    echo "Existing self-signed certificates found."
+fi
+
+# The pre_hook links to the self-signed certificates
+PRE_HOOK="ln -fs ./self-signed/privkey.pem /etc/letsencrypt/privkey.pem && ln -fs ./self-signed/cert.pem /etc/letsencrypt/cert.pem"
+
+# The post_hook relinks the certificates (to replace self-signed ones) and then runs the provided HOOK
+DEPLOY_HOOK="ln -fs ./live/${MODE}/privkey.pem /etc/letsencrypt/privkey.pem && ln -fs ./live/${MODE}/cert.pem /etc/letsencrypt/cert.pem && $HOOK"
 
 if [ "$MODE" = "disabled" ]; then
 
     echo "YOU ARE IN DISABLED MODE. Set MODE=staging or MODE=production to get a letsencrypt certificate."
+
+    # We juse a placeholder command, showing what would happen if mode was staging or production
     COMMAND="echo '*** certbot would run now if MODE was staging or production. ***'"
 
 else
 
-    # The hook relinks the certificates (to replace self-signed ones)
-    FULL_HOOK="ln -fs ./live/${MODE}/privkey.pem /etc/letsencrypt/privkey.pem && ln -fs ./live/${MODE}/cert.pem /etc/letsencrypt/cert.pem"
-    if [ ! -z "$HOOK" ]; then
-        FULL_HOOK="$FULL_HOOK && $HOOK"
-    fi
+    COMMAND="certbot certonly --cert-name ${MODE} --webroot --webroot-path /challenges --non-interactive --agree-tos -m ${EMAIL} -d ${DOMAIN} --pre-hook '$PRE_HOOK' --deploy-hook '$DEPLOY_HOOK'"
 
-    COMMAND="certbot certonly --cert-name ${MODE} --webroot --webroot-path /challenges --non-interactive --agree-tos -m ${EMAIL} -d ${DOMAIN} --deploy-hook '$FULL_HOOK'"
     if [ "$MODE" != "production" ]; then
         echo "YOU ARE IN STAGING MODE. Set MODE=production to generate a real certificate."
         COMMAND="$COMMAND --staging"
@@ -47,20 +51,38 @@ else
 
 fi
 
-echo "We run the command once (initial check)..."
+# We must manually run the PRE_HOOK to link to the self-signed certificates in case
+# mode was changed to "disabled"
+if [ "$MODE" = "disabled" ]; then
+    eval "$PRE_HOOK"
+fi
+
+echo "We run the command once to make sure it works..."
+set +e
 eval "$COMMAND"
+set -e
 
-echo "First sync was successful!"
+if [ $? -ne 0 ]; then
+    echo "FAILURE !"
+    if [ "$MODE" = "production" ]; then
+        echo "Waiting for 60 seconds to avoid hitting letsencrypt limit too quickly"
+        sleep 60
+    fi
+    exit 1
+else
+    echo "SUCCESS !"
+fi
 
-echo "We run the hook..."
-eval "$FULL_HOOK"
+# We must manually run the DEPLOY_HOOK too in case mode was switched back to "staging" or "production"
+# but the certificates were not due for renewal.
+if [ "$MODE" != "disabled" ]; then
+    eval "$DEPLOY_HOOK"
+fi
 
-echo "And prepare the following cronjob:"
-CRONJOB="0 * * * * $COMMAND"
+echo "Preparing the following cronjob :"
+CRONJOB="0 */6 * * * $COMMAND"
 echo "$CRONJOB"
-
-echo "Installing the cron job..."
 echo "$CRONJOB" > /etc/crontabs/root
 
-echo "Starting..."
+echo "Starting cron..."
 exec "$@"
